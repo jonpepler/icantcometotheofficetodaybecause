2ed316d01c50c6ed13f31e5ca4f527f8fc03f2af
Jon <jonathan.pepler@gmail.com>
2024-09-04 17:57:24 +0100

    fix refresh with rapid change 'feature'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { createExcuse } from "@/util/createExcuse";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [excuse, setExcuse] = useState(createExcuse());
+
+  const newExcuse = () => setExcuse(createExcuse());
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("beforeunload", newExcuse);
+      return () => window.removeEventListener("beforeunload", newExcuse);
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1>i cant come to the office today because</h1>
-      <h1>{createExcuse()}.</h1>
+      <h1 suppressHydrationWarning>{excuse}.</h1>
     </main>
   );
 }
